808e59ad8023636dc6dacb05f814683bb22dbb17
Ireoo <s@ireoo.com>
2016-03-25 02:13:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-var io = requit('socket-client');
+var db = require('./lib/db'),
+	io = require('socket.io-client');
+
+var socket = io('https://websocket.btcc.com/');
+socket.emit('subscribe', 'marketdata_cnybtc');
+socket.emit('subscribe', 'marketdata_cnyltc');
+socket.emit('subscribe', 'marketdata_btcltc');
+socket.emit('subscribe', 'grouporder_cnybtc');
+socket.emit('subscribe', 'grouporder_cnyltc');
+socket.emit('subscribe', 'grouporder_btcltc');
+socket.on('connect', function(){
+	socket.on('trade', function (data) {
+		console.log(data);
+	});
+	socket.on('ticker', function (data) {
+		console.log(data);
+	});
+	socket.on('grouporder', function (data) {
+		console.log(data);
+	});
+});
+
